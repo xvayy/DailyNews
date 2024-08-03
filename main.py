5374579659f2dcfe04f20f913a3d0eb66368f0b1
Yuliia Ivaniuk <ivanykjula@gmail.com>
2024-08-03 16:27:53 +0300

    Add links, subject, specify language, control topic

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
+
 api_key = "4ee2a7884a3041a2a66662555795b698"
-url = ("https://newsapi.org/v2/everything?q=tesla&" 
-       "from=2024-07-03&sortBy=publishedAt&apiKey="
-       "4ee2a7884a3041a2a66662555795b698")
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&" 
+       "from=2024-07-03&sortBy=publishedAt&"
+       "apiKey=4ee2a7884a3041a2a66662555795b698&"
+       "language=en")
 
 # Make a request
 req = requests.get(url)
@@ -17,9 +21,19 @@ print(type(content["articles"]))
 body = ""
 
 # Print title and author's name
-for article in content["articles"]:
-       body = body + article["title"] + "\n" + article["content"] + "\n" + str(article["author"]) + 2*"\n"
+for article in content["articles"][:3]:
+       body = "\n" + body + article["title"] + "\n" \
+              + article["description"] + "\n" \
+              + str(article["author"]) + "\n" \
+              + article["url"] + 2*"\n"
+
+letter = f"""\
+Subject: Today's News
+{body}
+"""
+
+
 print(body)
 
-body = body.encode("utf-8")
-send_email(body)
+letter = letter.encode("utf-8")
+send_email(letter)
